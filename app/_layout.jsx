0ae02b2f975d7ffd5ed8410be067600a2fdce199
eagle9111 <alhassan.khalilnew@gmail.com>
2025-04-25 21:45:56 +0300

    web

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,9 +2,15 @@ import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { Slot } from 'expo-router'
 
+// Get the Clerk public API URL from environment variables
+const clerkFrontendApi = process.env.EXPO_PUBLIC_CLERK_FRONTEND_API;
+
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkProvider 
+      frontendApi={clerkFrontendApi} 
+      tokenCache={tokenCache}
+    >
       <Slot />
     </ClerkProvider>
   )
